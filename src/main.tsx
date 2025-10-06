1b964c337449bb47e2ff3d3d244412899165a1cd
Edan Harr <edan@analyticintelligencesolutions.com>
2025-10-05 23:31:06 -0700

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
-import { ProfileProvider } from './contexts/ProfileContext';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ProfileProvider>
-      <App />
-    </ProfileProvider>
+    <App />
   </StrictMode>
 );
